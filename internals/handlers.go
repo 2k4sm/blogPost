a1e241aee223806055836f138226a3013369e2c8
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-09-23 22:54:08 +0530

    route added for form processing.

--- internals/handlers.go
+++ internals/handlers.go
@@ -1,6 +1,8 @@
 package internals
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -13,6 +15,9 @@ func ViewAll(c *fiber.Ctx) error {
 	if err != nil {
 		panic("failed to connect Database.")
 	}
+	db.AutoMigrate(Post{})
+
+	//db.Create(&Post{Title: "md test", Author: "md author", Description: "### working with md\n # this is a big tag\n - point1\n - point2\n [link](https://www.google.com)"})
 
 	var posts []*Post
 	rows, err := db.Model(&Post{}).Rows()
@@ -36,4 +41,23 @@ func ViewAll(c *fiber.Ctx) error {
 
 func CreatePost(c *fiber.Ctx) error {
 	return c.SendString("hello")
+}
+
+func ProcessForm(c *fiber.Ctx) error {
+
+	db, err := gorm.Open(sqlite.Open("posts.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect Database.")
+	}
+
+	title := c.FormValue("title")
+	author := c.FormValue("author")
+	desc := c.FormValue("description")
+	post := &Post{Title: title, Author: author, Description: desc}
+	db.Create(post)
+
+	fmt.Println("Form Processed Successfully.")
+
+	return c.RedirectToRoute("/", nil)
 }
