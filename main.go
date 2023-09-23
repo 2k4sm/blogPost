2daf9a169563e47546ad5ed31482b1cac79c61f6
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-09-23 20:39:48 +0530

    configured server to improve safety

--- main.go
+++ main.go
@@ -11,13 +11,18 @@ func main() {
 	engine := html.New("./ui/html", ".html")
 
 	app := fiber.New(fiber.Config{
-		Views: engine,
+		Views:         engine,
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "blogPost",
+		AppName:       "blogPost v0.0.1",
 	})
 
 	//app.Static("/static", "../../ui/html")
 
 	app.Get("/", internals.ViewAll)
-
+	app.Post("/createpost", internals.CreatePost)
 	app.Listen(":8000")
 
 }
