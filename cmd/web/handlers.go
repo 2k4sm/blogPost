103e5dd94f593444546f2c0d36b81723e50b55d6
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-09-23 11:36:18 +0530

    created views handler

---
+++ cmd/web/handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/2k4sm/blogPost/internals/model"
+	"github.com/gofiber/fiber/v2"
+	"github.com/objectbox/objectbox-go/objectbox"
+)
+
+func initObjectBox() *objectbox.ObjectBox {
+	objectBox, err := objectbox.NewBuilder().Model(model.ObjectBoxModel()).Build()
+
+	if err != nil {
+		panic(fmt.Sprintf("Error initialising Object Box: %v", err))
+	}
+
+	return objectBox
+}
+
+func viewAll(c *fiber.Ctx) error {
+	ob := initObjectBox()
+	defer ob.Close()
+
+	box := model.BoxForPost(ob)
+
+	posts, _ := box.GetAll()
+
+	p, err := json.Marshal(posts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.Response().BodyWriter().Write([]byte(p))
+
+	return nil
+}
