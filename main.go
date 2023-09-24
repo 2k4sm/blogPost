52fa399fcbe852320167e846fc91148b24b5779d
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-09-24 15:03:48 +0530

    removed env variables

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/2k4sm/blogPost/internals"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
@@ -25,8 +22,7 @@ func main() {
 	app.Static("/", "./ui/assets")
 
 	app.Get("/", internals.RenderHome)
-	key := os.Getenv("API_KEY")
-	app.Get(fmt.Sprintf("/api/%s/views", key), internals.ViewAll)
+	app.Get("/api/views", internals.ViewAll)
 	app.Get("/createpost", internals.CreatePost)
 	app.Post("/createpost", internals.ProcessForm)
 	app.Listen(":8000")
