c2dda0dbffed6bc242c44d2930c3780a7a7cbad5
Prem B. Shah <techie.prembshah@gmail.com>
2023-09-24 13:01:02 +0530

    This optimizes the image addition functionality

--- ui/assets/publish.js
+++ ui/assets/publish.js
@@ -9,3 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
       markdownPreview.innerHTML = marked.parse(markdownText);
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+          const img = mutation.target.querySelector('img');
+          if (img) {
+              img.style.height = '10vh';
+          }
+      });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+});
